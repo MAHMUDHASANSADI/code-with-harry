ee99a64f3301b320911fd74294c69a884d398f05
unknown <mhasan181075@bscse.uiu.ac.bd>
2024-10-03 11:17:46 +0600

    consdition check done

--- conditional_expressions/conditional.js
+++ conditional_expressions/conditional.js
@@ -1,5 +1,14 @@
 let a= prompt("whats your age?");
 console.log(a);
 
-a= Number.parseInt('a');
-console.log(typeof a);
+if(a<0)
+{
+    console.log("your are a baccha");
+}
+else if(a<18 && a>9)
+{
+    console.log("you can drive after 18");
+}
+else {
+    console.log("now you can drive");
+}
