8c01ea3164994614120c8cc13271a8fce91c2077
unknown <mhasan181075@bscse.uiu.ac.bd>
2024-12-01 14:15:19 +0600

    object using done

---
+++ nestedObj.js
@@ -0,0 +1,9 @@
+const person={
+    name:"sadi",
+    id: 011181075,
+    car:{
+        color:"blue"
+    }
+};
+
+console.log(person.car.color);
---
+++ object.js
@@ -0,0 +1,7 @@
+const sadi = {
+    degree: 'Bsc in CSE',
+    age: 26,
+    profession: 'software engineer'
+}
+console.log(sadi.age);
+console.log(sadi);
